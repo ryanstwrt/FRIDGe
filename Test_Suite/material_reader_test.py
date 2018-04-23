f37c252eac238068ce8abf77088028e6dc7e3a05
Ryan Stewart <stewryan@isu.edu>
2018-04-22 17:23:48 -0700

    Added multi-isotope test for Uranium (4 isotopes).

--- Test_Suite/material_reader_test.py
+++ Test_Suite/material_reader_test.py
@@ -7,7 +7,7 @@ def test_element_input():
      different cases. One with one isotope, one with two isotopes and one with
      four isotopes to check scalability."""
 
-    # One element reader for Sodium
+    # One isotope reader for Sodium
     cur_dir = os.path.dirname(__file__)
     elem_dir = os.path.join(cur_dir, '../CotN/Na.txt')
     elem = mat_read.element_input(elem_dir)
@@ -21,6 +21,33 @@ def test_element_input():
     assert na_mass_num == elem[0][2]
     assert na_abun == elem[0][3]
     assert na_density == elem[0][4]
+
+    # Two isotope reader for
+
+    # Multi isotope reader for Uranium
+    elem_dir = os.path.join(cur_dir, '../CotN/U.txt')
+    elem = mat_read.element_input(elem_dir)
+
+    # Check element number
+    assert elem[0][0] == 92000
+    # Check ZAID number
+    assert elem[0][1] == 92234
+    assert elem[1][1] == 92235
+    assert elem[2][1] == 92236
+    assert elem[3][1] == 92238
+    # Check mass number
+    assert elem[0][2] == 234.0409504
+    assert elem[1][2] == 235.0439282
+    assert elem[2][2] == 236.0455662
+    assert elem[3][2] == 238.0507870
+    # Check natural abundance
+    assert elem[0][3] == 0.000054
+    assert elem[1][3] == 0.007204
+    assert elem[2][3] == 0.000000
+    assert elem[3][3] == 0.992742
+    # Check element density
+    assert elem[0][4] == 18.95
+
     return
 
 test_element_input()

b112154dbb61a146034bdeb093e7f23e57addbb2
Ryan Stewart <stewryan@isu.edu>
2018-05-03 16:48:58 -0700

    Created frame for pin maker. Created two functions to create RCC and RHP mcnp surfaces. Tests built for mcnp surface creators, but not for pin maker yet.

---
+++ fridge/Test_Suite/pin_maker_test.py
@@ -0,0 +1,19 @@
+from FRIDGe.fridge.driver import pin_maker as pm
+
+def test_mcnp_macro_RCC():
+    output, warning = pm.mcnp_make_macro_RCC(1, [0,0,0], [0,0,10], 0.5, 'Testing to make sure')
+    assert len(output) < 80
+    assert warning == False
+    output, warning = pm.mcnp_make_macro_RCC(1, [0,0,0], [0,0,10], 0.5, 'Testing to make sure it will be longer than 80 characters so it will fail the test')
+    assert warning == True
+
+
+def test_mcnp_macro_RHP():
+    output, warning = pm.mcnp_make_macro_RCC(1, [0,0,0], [0,0,10], [0, 0.5, 0], 'Testing to make sure')
+    assert len(output) < 80
+    assert warning == False
+    output, warning = pm.mcnp_make_macro_RCC(1, [0,0,0], [0,0,10], [0, 0.5, 0], 'Testing to make sure it will be longer than 80 characters so it will fail the test')
+    assert warning == True
+
+test_mcnp_macro_RCC()
+test_mcnp_macro_RHP()
--- fridge/driver/fridge_driver.py
+++ fridge/driver/fridge_driver.py
@@ -1,6 +1,7 @@
 from FRIDGe.fridge.input_readers import material_reader as mat_read
 from FRIDGe.fridge.input_readers import geometry_reader as geo_read
 from FRIDGe.fridge.driver import assembly_holder as ah
+from FRIDGe.fridge.driver import pin_maker
 
 print('Welcome to FRIDGe, the Fast Reactor Input Deck Generator!')
 assembly_type = 'A271'# input('Please input the assembly type you would like to model: ')
@@ -12,8 +13,7 @@ fuel_material_fuel = mat_read.material_reader([fuel.ix['fuel', 'fuel']])
 fuel_material_bond = mat_read.material_reader([fuel.ix['bond', 'fuel']])
 fuel_material_cladding = mat_read.material_reader([fuel.ix['clad', 'fuel']])
 
-fuel_assembly = ah.Assembly(assembly, 'fuel')
+fuel_assembly = ah.Assembly(assembly, 'fuel', [fuel, fuel_material_fuel, fuel_material_bond, fuel_material_cladding])
+fuel_pin = pin_maker.fuel_pin_maker(fuel_assembly)
 
 
-
-print(fuel_pin.pin_data)

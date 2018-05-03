e43e20e09cfc957303a31b3340ddb407be4038d3
Ryan Stewart <stewryan@isu.edu>
2018-05-03 13:16:55 -0700

    Added elements for HT-9 stainless steel.

--- fridge/driver/fridge_driver.py
+++ fridge/driver/fridge_driver.py
@@ -6,6 +6,7 @@ assembly_type = input('Please input the assembly type you would like to model: '
 
 fuel, assembly, plenum, fuel_reflector = geo_read.fuel_assembly_geometry_reader(assembly_type)
 
+
 fuel_material_fuel = mat_read.material_reader([fuel.ix['fuel', 'fuel']])
 fuel_material_bond = mat_read.material_reader([fuel.ix['bond', 'fuel']])
 fuel_material_cladding = mat_read.material_reader([fuel.ix['clad', 'fuel']])

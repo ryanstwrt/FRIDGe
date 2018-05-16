e5608c00ae24b582cc12b55bf73439ca8a539750
Ryan Stewart <stewryan@isu.edu>
2018-05-15 18:00:15 -0700

    Finished material_smear function and tests

--- fridge/Test_Suite/material_smear_test.py
+++ fridge/Test_Suite/material_smear_test.py
@@ -1,12 +1,20 @@
 from FRIDGe.fridge.utilities import material_smear as ms
+from FRIDGe.fridge.input_readers import material_reader as mr
+import numpy as np
 
 
 def test_material_smear():
-#    atom_percent, atom_density = ms.material_smear([1, 2], [1, 2], ['27U'])
 
-    atom_percent, atom_density = ms.material_smear([1, 2], [0.3, 0.7], ['Liquid_Na', 'HT9'])
-    print(atom_percent)
-    print(atom_density)
+    #test a material smear where there is only one material
+    atom_percent, atom_density = ms.material_smear([1.0], ['27U'])
+    true_atom_percent, true_atom_density = mr.material_reader(['27U'])
+    assert atom_percent.all() == true_atom_percent.all()
+    assert atom_density == true_atom_density
 
+    atom_percent1, atom_density1 = ms.material_smear([0.3, 0.7], ['Liquid_Na', 'HT9'])
+    atom_percent2, atom_density2 = ms.material_smear([0.7, 0.3], ['HT9', 'Liquid_Na'])
+
+    assert np.allclose(atom_density1, atom_density2)
+    assert atom_percent1.all() == atom_percent2.all()
 
 test_material_smear()
--- fridge/utilities/material_smear.py
+++ fridge/utilities/material_smear.py
@@ -8,7 +8,7 @@ cur_dir = os.path.dirname(__file__)
 mat_dir = os.path.join(cur_dir, "../Materials")
 
 
-def material_smear(materials, material_wt_per, material_str):
+def material_smear(material_wt_per, material_str):
     assert material_str
     material_name = ''
     for iter, name in enumerate(material_str):
@@ -16,38 +16,43 @@ def material_smear(materials, material_wt_per, material_str):
             material_name += str(name)
         else:
             material_name += str(name) + '_'
-    print(material_name)
-    # Check to see if the material smear already exists
     material_dir = '../Materials/' + material_name + '.txt'
     material_exist = os.path.isfile(material_dir)
-    if material_exist:
-        atom_percent, atom_density = mr.material_reader([material_name])
-        return atom_percent, atom_density
+    #if material_exist:
+    #    atom_percent, atom_density = mr.material_reader([material_name])
+    #    return atom_percent, atom_density
 
-    material_creator(material_wt_per, material_str)
-    #dummy functions for now
-    return 1, 2
+    wt_per = material_creator(material_wt_per, material_str)
+    atom_percent, atom_density = mr.wt2at_per(wt_per, [wt_per[0, 4]])
+    return atom_percent, atom_density
 
 def material_creator(material_wt_per, material_str):
     """
-
+        Augments the wt
 
     """
+    smear_density = 0
     for iter, material_name in enumerate(material_str):
         wt_per, material_density = mr.get_final_wt_per([material_name])
-        print(wt_per)
         temp, temp1 = mr.material_reader([material_name])
         temp = pd.DataFrame(temp, columns=['element', 'zaid', 'mass_number', 'wt_per', 'density', 'linear expansion'])
         temp.wt_per = wt_per[:, 3] * material_wt_per[iter]
         temp.density = material_density
+
+        # create a uniform density using the law of mixtures a_12 = a_1 * wt%_1 + a2 * wt%_2 + ...
+        smear_density += material_density * material_wt_per[iter]
+
         if iter == 0:
             material_array = temp
         else:
             material_array = pd.concat([material_array, temp])
 
+    material_array.density = smear_density
+
     if round(material_array['wt_per'].sum(), 15) != 1.0:
         print('\033[1;37:33mWarning: Smear material with %s had a weight fraction of %f and was not normlized to 1. '
               'Check to make sure the material or element card is correct' % (material_str, material_array['wt_per'].sum()))
+
     material_array = material_array.as_matrix()
 
     return material_array

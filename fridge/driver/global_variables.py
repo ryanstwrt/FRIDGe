5cbc805fd75f2033ed6f272d4b42b1e6db7f4e1b
Ryan Stewart <stewryan@isu.edu>
2018-05-30 15:36:06 -0700

    Added XC set to global_variables and input file.

--- fridge/driver/global_variables.py
+++ fridge/driver/global_variables.py
@@ -9,7 +9,8 @@ class global_variables():
     """
     def __init__(self, assembly_name):
         self.assembly_file_name = assembly_name + '_Assembly'
-        self.xc_set = '.82c'
+        self.xc_library = ''
+        self.xc_set = ''
         self.universe = 100
         self.number_assemblies = 1
         self.na_voiding = False
@@ -30,6 +31,7 @@ class global_variables():
 
         with open(assembly_file[0], "r") as file:
             inputs = yaml.safe_load(file)
+
         self.assembly_type = inputs["Assembly Type"]
         self.number_assemblies = int(inputs["Number of Assemblies"])
         self.na_voiding = bool(inputs["Na Voiding"])
@@ -38,9 +40,33 @@ class global_variables():
         self.temp_adjusted_volume = bool(inputs["Temperature Adjusted Volume"])
         self.clad_smear = bool(inputs["Smear Clad"])
         self.bond_smear = bool(inputs["Smear Bond"])
+        self.xc_set = inputs["XC Set"]
 
         if self.na_voiding:
             if "Void Percent" in inputs:
                 self.void_per = float(inputs["Void Percent"])
             else:
                 print("Sodium voiding requires 'Void Percent:' in the input file")
+
+        # Set the XC set depending on the temperature
+        if self.temperature == 600:
+            if self.xc_library == 'ENDFVII.1':
+                self.xc_set = '.81c'
+            elif self.xc_set == 'ENDFVII.0':
+                self.xc_set = '.71c'
+            elif self.xc_library == 'JEFF3.1':
+                self.xc_set = '.34c'
+        elif self.temperature == 900:
+            if self.xc_library == 'ENDFVII.1':
+                self.xc_set = '.82c'
+            elif self.xc_set == 'ENDFVII.0':
+                self.xc_set = '.72c'
+            elif self.xc_library == 'JEFF3.1':
+                self.xc_set = '.37c'
+        elif self.temperature == 1200:
+            if self.xc_library == 'ENDFVII.1':
+                self.xc_set = '.83c'
+            elif self.xc_set == 'ENDFVII.0':
+                self.xc_set = '.73c'
+            elif self.xc_library == 'JEFF3.1':
+                self.xc_set = '.39c'

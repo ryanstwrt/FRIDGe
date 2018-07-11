27949830be484c8f6905ed7e5ffa8144bb968ed8
Ryan Stewart <stewryan@isu.edu>
2018-07-11 05:53:34 -0700

    Update geometry reader to yaml format.

--- fridge/test_suite/geometry_reader_test.py
+++ fridge/test_suite/geometry_reader_test.py
@@ -5,7 +5,7 @@ def test_fuel_assembly_reader():
     """Tests the fuel assembly reader by reading in a known fuel
     assembly and ensure the values are accurate."""
 
-    fuel_data, assembly_data, plenum_data, fuel_reflector_data = geo_read.fuel_assembly_geometry_reader('A271_test')
+    fuel_data, assembly_data, plenum_data, fuel_reflector_data = geo_read.fuel_assembly_geometry_reader('A271_Test')
 
     # Test the fuel data
     assert 0.53 == fuel_data.ix['pin_diameter', 'fuel']
@@ -45,5 +45,3 @@ def test_fuel_assembly_reader():
     assert 'HT9' == fuel_reflector_data.ix['clad', 'fuel_reflector']
 
     return
-
-test_fuel_assembly_reader()
--- fridge/utilities/geometry_reader.py
+++ fridge/utilities/geometry_reader.py
@@ -26,11 +26,11 @@ def fuel_assembly_geometry_reader(assembly_type):
         fuel_reflector_data (Data Frame): Contains all the geometric and material components for the fuel reflector
 
     """
-    assembly_file = glob.glob(os.path.join(geo_dir, assembly_type + '.*'))
-    assembly_yaml_file = glob.glob(os.path.join(geo_dir, 'A271.yaml'))
+    assembly_yaml_file = glob.glob(os.path.join(geo_dir, assembly_type + '.yaml'))
+    #assembly_yaml_file = glob.glob(os.path.join(geo_dir, 'A271.yaml'))
 
 
-    if assembly_file == []:
+    if assembly_yaml_file == []:
         print('\n\033[1;37;31mFatal Error: No assembly type named %s. \nChange your assembly type to a previously created assembly, '
               'or create a new assembly using the utilities.' % assembly_type)
         quit()
@@ -46,87 +46,48 @@ def fuel_assembly_geometry_reader(assembly_type):
     fuel_reflector_materials = []
     fuel_reflector_materials_smears = np.zeros(2)
 
-    #with open(assembly_yaml_file[0], "r") as mat_file:
+    with open(assembly_yaml_file[0], "r") as mat_file:
 
-     #   inputs = yaml.load(mat_file)
-     #   fuel_diameter = float(inputs["Pin_Diameter"])
-     #   plenum_height = float(inputs['Plenum Height'])
-     #   plenum_smear = [float(i) for i in inputs['Plenum Smear']]
-     #   plenum_material = inputs["Plenum Material"]
-     #   plenum_Data = [plenum_height, plenum_smear[0], plenum_smear[1], plenum_smear[2], plenum_material[0], plenum_material[1], plenum_material[2]]
-     #   plenum_Data = pd.DataFrame(plenum_Data,
-     #                          columns=['plenum'],
-     #                          index=['height', 'coolant_per', 'void_per', 'clad_per', 'coolant', 'void', 'clad'])
+        inputs = yaml.load(mat_file)
+        fuel_diameter = float(inputs["Pin Diameter"])
+        clad_thickness = float(inputs['Clad Thickness'])
+        fuel_smear = float(inputs['Fuel Smear'])
+        fuel_pitch = float(inputs['Pitch'])
+        wire_wrap_diameter = float(inputs['Wire Wrap Diameter'])
+        height = float(inputs['Fuel Height'])
+        fuel = inputs['Fuel']
+        clad = inputs['Clad']
+        bond = inputs['Bond']
+        fuel_data = [fuel_diameter, clad_thickness, fuel_smear, fuel_pitch, wire_wrap_diameter, height, fuel, clad, bond]
+        fuel_data = pd.DataFrame(fuel_data, columns=['fuel'], index = ['pin_diameter', 'clad_thickness', 'fuel_smear', 'pitch',
+                                    'wire_wrap_diameter', 'height', 'fuel', 'clad', 'bond'])
 
-
-    with open(assembly_file[0], "r") as mat_file:
-        for i, line in enumerate(mat_file):
-            line = line.strip()
-            holder = [x for x in line.split(' ')]
-            if 0 < i < 10:
-                if i > 6:
-                    fuel_materials.append(holder[1])
-                else:
-                    fuel_data[i - 1] = holder[1]
-            elif 11 < i < 20:
-                if i > 17:
-                    assembly_materials.append(holder[1])
-                else:
-                    assembly_data[i - 12] = holder[1]
-            elif 21 < i < 25:
-                if i == 23:
-                    plenum_material_smears[0:] = holder[1:]
-                elif i == 24:
-                    plenum_materials = holder[1:]
-                elif i == 22:
-                    plenum_data[0] = holder[1]
-            elif 26 < i < 30:
-                if i == 29:
-                    fuel_reflector_materials = holder[1:]
-                elif i == 28:
-                    fuel_reflector_materials_smears[0:] = holder[1:]
-                else:
-                    fuel_reflector_data[0] = holder[1]
-
-    # Accumulate and organize the fuel data/materials
-    fuel_data = pd.DataFrame(fuel_data, columns=['fuel'],
-                             index=['pin_diameter', 'clad_thickness', 'fuel_smear', 'pitch',
-                                    'wire_wrap_diameter', 'height'])
-    fuel_materials = pd.DataFrame(fuel_materials, columns=['fuel'],
-                                  index=['fuel', 'clad', 'bond'])
-    fuel_data = pd.concat([fuel_data, fuel_materials])
-
-    # Accumulate and organize the assembly data/materials
-    assembly_data = pd.DataFrame(assembly_data,
-                                 columns=['assembly'],
-                                 index=['pins_per_assembly', 'assembly_pitch', 'duct_thickness',
-                                        'assembly_gap', 'inside_flat_to_flat', 'height'])
-    assembly_materials = pd.DataFrame(assembly_materials,
-                                      columns=['assembly'],
-                                      index=['coolant', 'assembly'])
-    assembly_data = pd.concat([assembly_data, assembly_materials])
-
-    # Accumulate and organize the plenum data/materials
-    plenum_data = np.append(plenum_data, plenum_material_smears)
-
-    plenum_data = pd.DataFrame(plenum_data,
+        plenum_height = float(inputs['Plenum Height'])
+        plenum_smear = [float(i) for i in inputs['Plenum Smear']]
+        plenum_material = inputs["Plenum Material"]
+        plenum_data = [plenum_height, plenum_smear[0], plenum_smear[1], plenum_smear[2], plenum_material[0], plenum_material[1], plenum_material[2]]
+        plenum_data = pd.DataFrame(plenum_data,
                                columns=['plenum'],
-                               index=['height', 'coolant_per', 'void_per', 'clad_per'])
-    plenum_materials = pd.DataFrame(plenum_materials,
-                                    columns=['plenum'],
-                                    index=['coolant', 'void', 'clad'])
-    plenum_data = pd.concat([plenum_data, plenum_materials])
+                               index=['height', 'coolant_per', 'void_per', 'clad_per', 'coolant', 'void', 'clad'])
 
-    # Accumulate and organize the fuel reflector data/materials
-    fuel_reflector_data = np.append(fuel_reflector_data, fuel_reflector_materials_smears)
+        fuel_reflector_height = float(inputs['Fuel Reflector Height'])
+        fuel_reflector_smear = [float(i) for i in inputs['Smear']]
+        fuel_reflector_material = inputs['Material']
+        fuel_reflector_data = [fuel_reflector_height, fuel_reflector_smear[0], fuel_reflector_smear[1], fuel_reflector_material[0], fuel_reflector_material[1]]
+        fuel_reflector_data = pd.DataFrame(fuel_reflector_data, columns=['fuel_reflector'],
+                                           index=['height', 'coolant_per', 'clad_per', 'coolant', 'clad'])
 
-    fuel_reflector_data = pd.DataFrame(fuel_reflector_data,
-                                       columns=['fuel_reflector'],
-                                       index=['height', 'coolant_per', 'clad_per'])
-    fuel_reflector_materials = pd.DataFrame(fuel_reflector_materials,
-                                            columns=['fuel_reflector'],
-                                            index=['coolant', 'clad'])
-    fuel_reflector_data = pd.concat([fuel_reflector_data, fuel_reflector_materials])
+        pins_per_assembly = float(inputs['Pins Per Assembly'])
+        assembly_pitch = float(inputs['Assembly Pitch'])
+        duct_thickness = float(inputs['Duct Thickness'])
+        assembly_gap = float(inputs['Assembly Gap'])
+        duct_inner_flat_to_flat = float(inputs['Duct Inside Flat to Flat'])
+        assembly_height = float(inputs['Assembly Height'])
+        coolant = inputs['Coolant']
+        assembly_material = inputs['Assembly Material']
+        assembly_data = [pins_per_assembly, assembly_pitch, duct_thickness, assembly_gap, duct_inner_flat_to_flat,
+                         assembly_height, coolant, assembly_material]
+        assembly_data = pd.DataFrame(assembly_data, columns=['assembly'], index=['pins_per_assembly', 'assembly_pitch', 'duct_thickness',
+                                        'assembly_gap', 'inside_flat_to_flat', 'height', 'coolant', 'assembly'])
 
-
-    return fuel_data, assembly_data, plenum_data, fuel_reflector_data
+        return fuel_data, assembly_data, plenum_data, fuel_reflector_data

54b8e5bd011f1e50e8d883ed4fada13d09040192
Ryan Stewart <stewryan@isu.edu>
2018-07-11 06:04:39 -0700

    Update geometry reader to yaml format.

--- fridge/test_suite/assembly_holder_test.py
+++ fridge/test_suite/assembly_holder_test.py
@@ -34,7 +34,7 @@ def test_assembly():
 
 def test_pin():
     """ Test the creation of the pin class"""
-    assembly_type = 'A271_test'
+    assembly_type = 'A271_Test'
     fuel, assembly, plenum, fuel_reflector = geo_read.fuel_assembly_geometry_reader(assembly_type)
     fuel_material_fuel = mat_read.material_reader([fuel.ix['fuel', 'fuel']])
     fuel_material_bond = mat_read.material_reader([fuel.ix['bond', 'fuel']])

bba31ff960aed44b667f98fb244b2fcd65acbbba
ryanstwrt <stewryan@isu.edu>
2019-04-26 08:29:02 -0700

    Updated driver test.

--- fridge/test_suite/driver_test.py
+++ fridge/test_suite/driver_test.py
@@ -1,13 +1,23 @@
 import fridge.driver.fridge_driver as fd
 import glob
 import os
+import filecmp
+
 cur_dir = os.path.dirname(__file__)
 mcnp_dir = os.path.join(cur_dir, '../mcnp_input_files/')
 
 
-def test_fridge_driver():
+def test_fridge_driver_singleAssembly():
     """Test that the new files gets made to the right directory"""
     fd.main('A271_Assembly_Test')
-    knownTestFile = glob.glob(os.path.join(mcnp_dir, 'Test_Known.i'))
-    testFile = glob.glob(os.path.join(mcnp_dir, 'Test.i'))
-#    assert testFile == knownTestFile
+    knownTestFile = glob.glob(os.path.join(mcnp_dir, 'Prefab_Fuel_Assembly_Test.i'))
+    testFile = glob.glob(os.path.join(mcnp_dir, 'Fuel_Assembly_Test.i'))
+    assert filecmp.cmp(testFile[0], knownTestFile[0]) is True
+
+
+def test_fridge_driver_core():
+    """Test that the new files gets made to the right directory"""
+    fd.main('Full_Core_Test')
+    knownTestFile = glob.glob(os.path.join(mcnp_dir, 'Prefab_Full_Core_Test.i'))
+    testFile = glob.glob(os.path.join(mcnp_dir, 'Full_Core_Test.i'))
+    assert filecmp.cmp(testFile[0], knownTestFile[0]) is True
